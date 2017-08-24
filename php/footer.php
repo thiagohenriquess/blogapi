beeb61bca0078cacfc764e6ddc0f985217b09ca7
Thiago Henrique <thiagohenriq@gmail.com>
2017-08-24 01:39:29 -0300

    primeiro commit

---
+++ php/footer.php
@@ -0,0 +1,8 @@
+<?php
+echo '
+    <footer>
+        <p>Copyright © 2017 Thiago Henrique. Todos os Direitos Reservados</p>
+    </footer>
+  </body>
+</html>';
+?>
---
+++ php/header.php
@@ -0,0 +1,10 @@
+<?php
+echo'
+<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="UTF-8">
+      <title>Blog</title>
+    </head>
+    <body>
+ ?>
